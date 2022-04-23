4e3eaee36db73c49cfa4374d769ca1f76c9ca78d
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-04-23 23:56:15 +0200

    added sinus and cosinus lookup table generator

---
+++ lookup_sin.py
@@ -0,0 +1,15 @@
+# create sinus lookup tables
+import math
+
+STEP = 10
+
+print("float sinLookup[] = {\\")
+for i in range(STEP, 360, STEP):
+    lookup_element = math.sin(math.radians(i))
+    lookup_element = round(lookup_element, 5)
+
+    if i + STEP >= 360:
+        print("   {}\\".format(lookup_element))
+    else:
+        print("   {},\\".format(lookup_element))
+print("};")
